8f0762435f38384fb6c68fa3d6d705945083328f
Ninekiwi <srijanquay@gmail.com>
2025-11-07 19:25:16 +0530

    Removed all

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// next.config.js
-const path = require("path");
-/** @type {import('next').NextConfig} */
-module.exports = {
-  outputFileTracingRoot: path.join(__dirname), // keep tracing inside this project
-  eslint: { ignoreDuringBuilds: true },
-};
